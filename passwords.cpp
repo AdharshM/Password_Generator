376252ec5c4cb0f4b5d216937880d37d8e273f3e
Adharsh M <52920216+AdharshM@users.noreply.github.com>
2020-05-12 20:09:53 +0530

    Add files via upload

---
+++ passwords.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, k;
+void hello(){
+
+	cout << "Welcome To Password Generator \n";
+	cout << "Enter Length of Password you want, and how many passwords you want to generate : ";
+}
+vector <string> v(k);
+void generate(){
+	char a='!';
+	string s="";
+	while(k--){
+		for(int i=0;i<n;i++){
+			s+=(a+rand()%94);
+		}
+		v.emplace_back(s);
+		s="";
+	}
+}
+int main(){
+	hello();
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n >> k;
+	generate();
+	ofstream MyFile("Password"+to_string(rand()%10000007)+".txt" );
+	for(int i=0;i<v.size();i++){
+		MyFile << v[i];
+		MyFile << "\n";
+	}
+	MyFile.close();
+	return 0;
+}
